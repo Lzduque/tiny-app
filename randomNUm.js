d2f14baeafdee367b05c9989df5b8afe11091c2d
Lzduque <lzduque@hotmail.com>
2019-04-19 18:04:50 +0000

    resolve shorturl redirection when long url has no http://

--- randomNUm.js
+++
@@ -1,7 +0,0 @@
-function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-    return result;
-}
-var rString = randomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-console.log(rString);
